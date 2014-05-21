c06642abaea1a9a59feac439f0c23bbd214818be
MSco <martin.schober@gmx.de>
2014-05-21 15:39:47 +0200

    Notify user if version is not supported

--- src/realperfectidling.js
+++ src/realperfectidling.js
@@ -58,6 +58,10 @@
 var RPI = {};
 
 RPI.supportedVersion = "1.0465"
+if (RPI.supportedVersion != Game.version)
+{
+	Game.Notify('Unsupported version','Real Perfect Idling has not been tested on this version of Cookie Clicker. Continue on your own peril!',[3,5],6);
+}
 
 RPI.calcGCSpawnTime = function()
 {

c672cfe83ae9c5a22d4fde1f83f696452f6b8c8f
MSco <martin.schober@gmx.de>
2014-05-22 13:25:09 +0200

    Game.T is substracted from afk time

--- src/realperfectidling.js
+++ src/realperfectidling.js
@@ -304,7 +304,7 @@ RPI.framesToString = function(time)
 
 if (!idleDone)
 {
-	var secondsAfk = (new Date().getTime()-Game.lastDate)/1000;
+	var secondsAfk = (new Date().getTime()-Game.lastDate)/1000 - Game.T/Game.fps;
 	//var secondsAfk = 50*60; 					// for debug
 	var framesAfk = secondsAfk*Game.fps;
 

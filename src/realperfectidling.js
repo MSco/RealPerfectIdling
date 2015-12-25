b61ed2d86227a22ad85cfc1f708133e7279f20d3
MSco <martin.schober@gmx.de>
2015-12-25 09:45:38 +0100

    Version 0.9.8.6

--- src/realperfectidling.js
+++ src/realperfectidling.js
@@ -1,7 +1,7 @@
 /* ================================================
     MSco Perfect Idling With Wrinklers - A Cookie Clicker plugin
 
-    Version: 0.9.8.5
+    Version: 0.9.8.6
     GitHub:  https://github.com/MSco/RealPerfectIdling
     Author:  Martin Schober
     Email:   martin.schober@gmx.de

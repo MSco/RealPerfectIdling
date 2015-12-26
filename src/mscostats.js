f61f660dcbb58168cf09dca29030c65f6e2c7b88
MSco <martin.schober@gmx.de>
2015-12-26 10:19:42 +0100

    Version: 0.9.10.3

--- src/mscostats.js
+++ src/mscostats.js
@@ -1,7 +1,7 @@
 /* ================================================
     MSco Cookie Stats - A Cookie Clicker plugin
 
-    Version: 0.9.10.2
+    Version: 0.9.10.3
     GitHub:  https://github.com/MSco/RealPerfectIdling
     Author:  Martin Schober
     Email:   martin.schober@gmx.de

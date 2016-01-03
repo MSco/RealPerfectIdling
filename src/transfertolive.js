ee6bf5af650a88aa37daffb0e9426b32ef8bba13
MSco <martin.schober@gmx.de>
2016-01-03 11:44:09 +0100

    Remove TieredAchievements, Upgrades of TC, AC, Prism

--- src/transfertolive.js
+++ src/transfertolive.js
@@ -29,4 +29,61 @@ Game.Achievements['Be kind, rewind'].won=0
 Game.Achievements['Infinitesimal'].won=0
 Game.Achievements['A still more glorious dawn'].won=0
 
+// Remove Achievements: Time Machine, Antimatter condenser, Prism
+Game.Achievements['Time warp'].won=0
+Game.Achievements['Alternate timeline'].won=0
+Game.Achievements['Rewriting history'].won=0
+Game.Achievements['Time duke'].won=0
+Game.Achievements['Forever and ever'].won=0
+Game.Achievements['Antibatter'].won=0
+Game.Achievements['Quirky quarks'].won=0
+Game.Achievements['It does matter'].won=0
+Game.Achievements['Molecular maestro'].won=0
+Game.Achievements['Walk the planck'].won=0
+Game.Achievements['Lone photon'].won=0
+Game.Achievements['Dazzling glimmer'].won=0
+Game.Achievements['Blinding flash'].won=0
+Game.Achievements['Unending glow'].won=0
+Game.Achievements['Rise and shine'].won=0
+
+// Remove Upgrades: Time Machine, Antimatter condenser, Prism
+// bought
+Game.Upgrades['Flux capacitors'].bought=0
+Game.Upgrades['Time paradox resolver'].bought=0
+Game.Upgrades['Quantum conundrum'].bought=0
+Game.Upgrades['Time duke'].bought=0
+Game.Upgrades['Forever and ever'].bought=0
+Game.Upgrades['Far future enactment'].bought=0
+Game.Upgrades['Antibatter'].bought=0
+Game.Upgrades['Quirky quarks'].bought=0
+Game.Upgrades['It does matter!'].bought=0
+Game.Upgrades['Molecular maestro'].bought=0
+Game.Upgrades['Walk the planck'].bought=0
+Game.Upgrades['Nanocosmics'].bought=0
+Game.Upgrades['Gem polish'].bought=0
+Game.Upgrades['9th color'].bought=0
+Game.Upgrades['Chocolate light'].bought=0
+Game.Upgrades['Grainbow'].bought=0
+Game.Upgrades['Pure cosmic light'].bought=0
+Game.Upgrades['Glow-in-the-dark'].bought=0
+// unlocked
+Game.Upgrades['Flux capacitors'].unlocked=0
+Game.Upgrades['Time paradox resolver'].unlocked=0
+Game.Upgrades['Quantum conundrum'].unlocked=0
+Game.Upgrades['Time duke'].unlocked=0
+Game.Upgrades['Forever and ever'].unlocked=0
+Game.Upgrades['Far future enactment'].unlocked=0
+Game.Upgrades['Antibatter'].unlocked=0
+Game.Upgrades['Quirky quarks'].unlocked=0
+Game.Upgrades['It does matter!'].unlocked=0
+Game.Upgrades['Molecular maestro'].unlocked=0
+Game.Upgrades['Walk the planck'].unlocked=0
+Game.Upgrades['Nanocosmics'].unlocked=0
+Game.Upgrades['Gem polish'].unlocked=0
+Game.Upgrades['9th color'].unlocked=0
+Game.Upgrades['Chocolate light'].unlocked=0
+Game.Upgrades['Grainbow'].unlocked=0
+Game.Upgrades['Pure cosmic light'].unlocked=0
+Game.Upgrades['Glow-in-the-dark'].unlocked=0
+
 Game.ExportSave()

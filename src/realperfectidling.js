475910eedd6ce438e1d3f377b87b9281df3eba02
MSco <martin.schober@gmx.de>
2016-01-03 13:44:54 +0100

    More output in rpi timeoffline

--- src/realperfectidling.js
+++ src/realperfectidling.js
@@ -422,8 +422,12 @@ RPI.undoOfflineEarned = function()
 		var timeOffline=(MS.importSaveDate-Game.lastDate)/1000;
 		console.log('RPI timeOffline: ' + timeOffline);
 		var timeOfflineOptimal=Math.min(timeOffline,maxTime);
+		console.log('RPI timeOfflineOptimal: ' + timeOfflineOptimal);
 		var timeOfflineReduced=Math.max(0,timeOffline-timeOfflineOptimal);
+		console.log('RPI timeOfflineReduced: ' + timeOfflineReduced);
 		var amount=(timeOfflineOptimal+timeOfflineReduced*0.1)*Game.cookiesPs*(percent/100);
+		console.log('RPI Game.cookiesPs: ' + Game.cookiesPs);
+		console.log('RPI amount: ' + amount);
 		
 		if (amount>0)
 		{

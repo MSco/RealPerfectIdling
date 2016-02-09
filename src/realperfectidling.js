13089790cf9479f4a9141d64da4cb859358442bb
MSco <martin.schober@gmx.de>
2016-02-09 09:13:08 +0100

    RPI Version: 1.0.2.2

--- src/realperfectidling.js
+++ src/realperfectidling.js
@@ -1,7 +1,7 @@
 /* ================================================
     MSco's Real Perfect Idling - A Cookie Clicker plugin
 
-    Version: 1.0.2.1
+    Version: 1.0.2.2
     GitHub:  https://github.com/MSco/RealPerfectIdling
     Author:  Martin Schober
     Email:   martin.schober@gmx.de

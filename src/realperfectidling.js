fb1671de565cfdbf41f69b4e3332b6d753db19dc
Martin Schober <m.schober@fz-juelich.de>
2022-01-05 23:05:35 +0100

    missedgolden: removed calculation for '5% shorter for every other golden cookie on the screen'

--- src/realperfectidling.js
+++ src/realperfectidling.js
@@ -78,7 +78,7 @@ RPI.addMissedGoldenCookies = function(durationFrames)
         if (Game.Has('Lucky payout')) dur*=1.01;
         
         dur *= ((3-Game.elderWrath) / 3.0 * Game.eff('goldenCookieDur')) + (Game.elderWrath / 3.0 * Game.eff('wrathCookieDur'));
-        dur *= Math.pow(0.95,Game.shimmerTypes['golden'].n-1);//5% shorter for every other golden cookie on the screen
+        //dur *= Math.pow(0.95,Game.shimmerTypes['golden'].n-1);//5% shorter for every other golden cookie on the screen
 	        
 		var thisMissed = Math.round(durationFrames/(RPI.calcGCSpawnTime()+dur))
 		Game.missedGoldenClicks += thisMissed;

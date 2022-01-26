107268349a80c7dba644b14b1f788114b8c11f81
MSco <martin.schober@gmx.de>
2022-01-26 23:32:58 +0100

    Msco stats 1.1.2.10

--- src/mscostats.js
+++ src/mscostats.js
@@ -14,7 +14,7 @@
 var MS = {};
 MS.Tooltip = {};
 
-MS.version = '1.1.2.9'
+MS.version = '1.1.2.10'
 
 // set MS.importSaveDate after importing a save, this is exclusively for another MSco Addon: Real Perfect Idling
 MS.importSaveDate = new Date().getTime() - Game.T*1000/Game.fps;

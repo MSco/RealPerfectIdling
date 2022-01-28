d9b6c5b7840d15054148173cd0ca4f5ddd923cd6
Martin Schober <m.schober@fz-juelich.de>
2022-01-29 00:30:01 +0100

    Disable F5 if lump type is golden, caramelized or if you got 7 lumps and next lump type is bifurcated

--- src/mscostats.js
+++ src/mscostats.js
@@ -14,7 +14,7 @@
 var MS = {};
 MS.Tooltip = {};
 
-MS.version = '1.1.3.3'
+MS.version = '1.1.3.4'
 
 // set MS.importSaveDate after importing a save, this is exclusively for another MSco Addon: Real Perfect Idling
 MS.importSaveDate = new Date().getTime() - Game.T*1000/Game.fps;
@@ -23,6 +23,7 @@ MS.pledgeT = 0;
 MS.heralds = 0;
 MS.offlineEarned = 0;
 MS.RPI_idledone = 0;
+MS.lastLumpsGained = 0
 MS.importSaveCodeOrignal = Game.ImportSaveCode;
 Game.ImportSaveCode = function(save)
 {
@@ -738,7 +739,8 @@ if(!statsdone && Game.sortedMods.length==0)
 	eval('Game.LoadSave='+Game.LoadSave.toString().replace("var amount=(timeOfflineOptimal+timeOfflineReduced*0.1)*Game.cookiesPs*(percent/100);", "var amount=(timeOfflineOptimal+timeOfflineReduced*0.1)*Game.cookiesPs*(percent/100); MS.offlineEarned=amount; "))
 	
 	// disable F5 if lump type == golden
-	eval('Game.computeLumpType='+Game.computeLumpType.toString().replace("Math.seedrandom();", "Math.seedrandom(); if (Game.lumpCurrentType==2||Game.lumpCurrentType==4) { document.addEventListener(\"keydown\", my_onkeydown_handler);}"));
+	eval('Game.gainLumps='+Game.gainLumps.toString().replace("if (Game.lumpsTotal==-1)","MS.lastLumpsGained=total; if (Game.lumpsTotal==-1)"))
+	eval('Game.computeLumpType='+Game.computeLumpType.toString().replace("Math.seedrandom();", "Math.seedrandom(); if (MS.lastLumpsGained==7 && Game.lumpCurrentType==1)||Game.lumpCurrentType==2||Game.lumpCurrentType==4) { document.addEventListener(\"keydown\", my_onkeydown_handler);}"));
 	
 	// reset grimoire stats after cast spell
 	castSpellOrig = Game.ObjectsById[7].minigame.castSpell;

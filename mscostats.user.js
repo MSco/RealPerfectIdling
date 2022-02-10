9897f5e46d07b98d850eb2195878ab2e7daa7af9
Martin Schober <m.schober@fz-juelich.de>
2022-02-10 13:12:21 +0100

    added mscostats.user.js

---
+++ mscostats.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name MSco Stats
+// @namespace MSco
+// @include http://orteil.dashnet.org/cookieclicker/
+// @include https://orteil.dashnet.org/cookieclicker/
+// @version 1.0.1
+// @grant none
+// ==/UserScript==
+
+(function() {
+    var checkReady = setInterval(function() {
+        if (typeof Game.ready !== 'undefined' && Game.ready) {
+            Game.LoadMod('https://rawgit.com/MSco/RealPerfectIdling/master/src/mscostats.js');
+            clearInterval(checkReady);
+        }
+    }, 500);
+})();
